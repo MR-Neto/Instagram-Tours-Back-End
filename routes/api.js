39bfcad480045c7d7dcb976bd54f4ef7356748a0
Miguel Ribeiro Neto <miguelribeironeto@MacBook-Pro-de-Miguel.local>
2019-03-02 16:24:18 +0100

    Added Stripe Backend and basic error handling

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const stripe = require('stripe')('sk_test_sHrxfQkR33g4YZQOoSNkbLEf');
 
 const router = express.Router();
 const Tour = require('../models/tour');
 const User = require('../models/user');
 const Place = require('../models/place');
+
 
 const fullCapacity = 4;
 const price = 25;
@@ -18,40 +20,68 @@ router.get('/tours', (req, res, next) => {
 });
 
 router.post('/book', async (req, res, next) => {
-  const { date, user, places } = req.body;
+  const { date, user, places } = req.body.details;
   let updatedTour;
   let createdTour;
 
   try {
-    const foundTour = await Tour.find({ date }).populate('users.buyer');
-    if (foundTour.length > 0) {
-      const availableSeats = fullCapacity - foundTour[0]
-        .users
-        .reduce((acc, currentUser) => acc + Number(currentUser.numberOfTickets), 0);
-      if (availableSeats >= user.numberOfTickets) {
-        updatedTour = await Tour.findOneAndUpdate({ date },
-          { $push: { users: user } },
-          { new: true });
+    // Set your secret key: remember to change this to your live secret key in production
+    // See your keys here: https://dashboard.stripe.com/account/apikeys
+
+    // Token is created using Checkout or Elements!
+    // Get the payment token ID submitted by the form:
+    const { token } = req.body;
+    console.log('token back end', token);
+    const charge = await stripe.charges.create({
+      amount: 999,
+      currency: 'eur',
+      description: 'Example charge',
+      source: 'tok_cvcCheckFail',
+    });
+
+    console.log('CHARGE ', charge);
+    if (charge) {
+      const foundTour = await Tour.find({ date }).populate('users.buyer');
+      if (foundTour.length > 0) {
+        const availableSeats = fullCapacity - foundTour[0]
+          .users
+          .reduce((acc, currentUser) => acc + Number(currentUser.numberOfTickets), 0);
+        if (availableSeats >= user.numberOfTickets) {
+          updatedTour = await Tour.findOneAndUpdate({ date },
+            { $push: { users: user } },
+            { new: true });
+        } else {
+          res.status(401);
+          res.json({ code: 'tour is full' });
+        }
       } else {
-        res.status(401);
-        res.json(foundTour);
+        const { buyer, numberOfTickets } = user;
+        createdTour = await Tour.create({
+          date,
+          users: [{
+            buyer,
+            numberOfTickets,
+          }],
+          places,
+          price,
+        });
       }
     } else {
-      const { buyer, numberOfTickets } = user;
-      createdTour = await Tour.create({
-        date,
-        users: [{
-          buyer,
-          numberOfTickets,
-        }],
-        places,
-        price,
-      });
+      console.log('CHARGE failed ', charge);
     }
+
     res.status(200);
-    res.json(updatedTour || createdTour);
+    res.json({
+      code: 'successful booking',
+      tour: updatedTour || createdTour,
+    });
   } catch (error) {
-    next(error);
+    if (error.type === 'StripeCardError') {
+      res.status(401);
+      res.json({ code: 'payment unsuccessful' });
+    } else {
+      next(error);
+    }
   }
 });
 
