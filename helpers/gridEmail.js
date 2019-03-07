1815ff29eb2b7a3134208eb3c4f26a3638330971
Nicolas <nicolas.lacaze@gmail.com>
2019-03-07 11:51:00 +0100

    Email test

--- helpers/gridEmail.js
+++ helpers/gridEmail.js
@@ -3,6 +3,7 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 function sendConfirmationEmail(to, from, subject, text) {
+  console.log('email sent');
   const msg = {
     to,
     from,
