f651ea8c791a3fdac44ab913b6b86e1a32b522e0
Miguel Ribeiro Neto <miguelribeironeto@MacBook-Pro-de-Miguel.local>
2019-03-07 11:50:18 +0100

    updated payment info

--- routes/api.js
+++ routes/api.js
@@ -35,7 +35,7 @@ router.post('/book', async (req, res, next) => {
       if (availableSeats >= numberOfTickets) {
         const { token } = req.body;
         const charge = await stripe.charges.create({
-          amount: 10,
+          amount: 1000,
           currency: 'eur',
           description: 'Instagram Tour',
           source: token,
@@ -64,7 +64,7 @@ router.post('/book', async (req, res, next) => {
     } else if (FULL_CAPACITY >= numberOfTickets) {
       const { token } = req.body;
       const charge = await stripe.charges.create({
-        amount: 10,
+        amount: 1000,
         currency: 'eur',
         description: 'Instagram Tour',
         source: token,
